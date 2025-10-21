12f436041473375d8e041d886624018ae7707f17
vasylhladyshev404 <vasyl@nextgear.nl>
2025-10-21 19:12:13 +0200

    1

--- services/order.py
+++ services/order.py
@@ -18,8 +18,16 @@ def create_order(
         dt = parse_datetime(date)
         if dt is None:
             raise ValueError(f"Invalid date format: {date}")
+
         order = Order(user=user, created_at=dt)
+
+        field = Order._meta.get_field("created_at")
+        old_auto_now_add = field.auto_now_add
+        field.auto_now_add = False
+
         order.save(force_insert=True)
+
+        field.auto_now_add = old_auto_now_add
     else:
         order = Order.objects.create(user=user)
 
